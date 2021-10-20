627ae1cb3f147bc66eb87bbea5210c9d32657ab7
Jinyu Meng <megabitsenmzq@gmail.com>
2021-10-21 00:33:55 +0900

    Fix login item.

--- Notch Simulator/Notch Simulator Helper/main.m
+++ Notch Simulator/Notch Simulator Helper/main.m
@@ -8,10 +8,25 @@
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
-    NSArray *pathComponents = [[[NSBundle mainBundle] bundlePath] pathComponents];
-    pathComponents = [pathComponents subarrayWithRange:NSMakeRange(0, [pathComponents count] - 4)];
-    NSString *path = [NSString pathWithComponents:pathComponents];
-    [[NSWorkspace sharedWorkspace] launchApplication:path];
+    
+    BOOL running = NO;
+    BOOL active = NO;
+    
+    NSArray *applications = [NSRunningApplication runningApplicationsWithBundleIdentifier:@"com.JinyuMeng.Notch-Simulator"];
+    if (applications.count > 0) {
+        NSRunningApplication *application = [applications firstObject];
+        
+        running = YES;
+        active = [application isActive];
+    }
+    
+    if (!running && !active) {
+        NSArray *pathComponents = [[[NSBundle mainBundle] bundlePath] pathComponents];
+        pathComponents = [pathComponents subarrayWithRange:NSMakeRange(0, [pathComponents count] - 4)];
+        NSString *path = [NSString pathWithComponents:pathComponents];
+        [[NSWorkspace sharedWorkspace] launchApplication:path];
+    }
+    [NSApp terminate:nil];
     return NSApplicationMain(argc, argv);
 }
 
