c9480b492e3bbf541d9828416beedf286633676a
Jinyu Meng <megabitsenmzq@gmail.com>
2021-10-22 14:35:21 +0900

    Edit promotion info.

--- Notch Simulator/Notch Simulator/MyAppsViewController.swift
+++ Notch Simulator/Notch Simulator/MyAppsViewController.swift
@@ -17,12 +17,17 @@ class MyAppsViewController: NSViewController {
     @IBAction func openTomoNowLink(_ sender: Any) {
         NSWorkspace.shared.open(URL(string: "https://apps.apple.com/us/app/id1505296579")!)
     }
+    
     @IBAction func openMagicShareLink(_ sender: Any) {
         NSWorkspace.shared.open(URL(string: "https://apps.apple.com/us/app/id1438149621")!)
     }
     
     @IBAction func openMDClockLink(_ sender: Any) {
         NSWorkspace.shared.open(URL(string: "https://apps.apple.com/app/apple-store/id1536358464?pt=119361776&ct=megbits&mt=8")!)
+    }
+    
+    @IBAction func openBaiMiaoLink(_ sender: Any) {
+        NSWorkspace.shared.open(URL(string: "https://baimiao.uzero.cn")!)
     }
     
     @IBAction func close(_ sender: Any) {
